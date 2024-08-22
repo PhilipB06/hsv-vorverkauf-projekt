3b1d072ce5f4dfcf59c0ef863fdd209323d9b9e7
Philip <76661774+PhilipB06@users.noreply.github.com>
2024-08-22 19:46:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ const fetchMatchData = async () => {
       if (preSaleDateMatchSimple) {
         const [_, preDay, preMonth, preYear] = preSaleDateMatchSimple;
         const preFullYear = preYear.length === 2 ? `20${preYear}` : preYear;
-        preSaleDate = new Date(`${preFullYear}-${preMonth}-${preDay}T10:00:00`);
+        preSaleDate = new Date(`${preFullYear}-${preMonth}-${preDay}T08:00:00`);
       }
     }
 
